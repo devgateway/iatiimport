077e96866f55e51562ad5bb04eee44d0a9c6c501
Fernando Ferreyra <fferreyra@developmentgateway.org>
2015-07-23 09:39:31 -0300

    IATIIT-12 Updated cookie name

--- import-core/import-ui/src/main/java/org/devgateway/importtool/main/Application.java
+++ import-core/import-ui/src/main/java/org/devgateway/importtool/main/Application.java
@@ -1,25 +1,49 @@
 package org.devgateway.importtool.main;
 
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan({ "org.devgateway.importtool.main", "org.devgateway.importtool.services", "org.devgateway.importtool.rest" })
+@ComponentScan({ "org.devgateway.importtool.main",
+		"org.devgateway.importtool.services", "org.devgateway.importtool.rest" })
 @Configuration
 @EnableAutoConfiguration
 public class Application extends WebMvcAutoConfiguration {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
 
-    @Bean
-    MultipartConfigElement multipartConfigElement() {
-        return new MultipartConfigElement("");
-    }
+	@Bean
+	public SessionTrackingConfigListener sessionTrackingConfigListener() {
+		SessionTrackingConfigListener listener = new SessionTrackingConfigListener();
+		return listener;
+	}
 
-} 
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		return new MultipartConfigElement("");
+	}
+
+	public class SessionTrackingConfigListener implements
+			ServletContextInitializer {
+
+		@Override
+		public void onStartup(ServletContext servletContext)
+				throws ServletException {
+			SessionCookieConfig sessionCookieConfig = servletContext
+					.getSessionCookieConfig();
+			sessionCookieConfig.setName("TOMCATSESSIONID");
+		}
+
+	}
+
+}
