f1fd4c9d2e1f314b10b52db1bfa76c06f2dd0801
Fernando Ferreyra <fferreyra@developmentgateway.org>
2015-07-23 09:49:30 -0300

    IATIIT-54

--- import-core/import-ui/src/main/webapp/client/scripts/components/wizard/choose-fields.jsx
+++ import-core/import-ui/src/main/webapp/client/scripts/components/wizard/choose-fields.jsx
@@ -7,6 +7,8 @@ var Link = Router.Link;
 var CustomSelect = require('./custom-select');
 var destinationFieldsStore = require('./../../stores/DestinationFieldsStore');
 var sourceFieldsStore = require('./../../stores/SourceFieldsStore');
+var fieldMappingStore = require('./../../stores/FieldMappingStore');
+var _ = require('lodash/dist/lodash.underscore');
 
 var ChooseFields = React.createClass({
     mixins: [
@@ -15,6 +17,7 @@ var ChooseFields = React.createClass({
     componentDidMount: function() {
         this.listenTo(destinationFieldsStore, this.updateDestinationFields);
         this.listenTo(sourceFieldsStore, this.updateSourceFields);
+        this.listenTo(fieldMappingStore, this.updateFieldMappingStore);
     },
     getInitialStateAsync: function() {
         appActions.loadDestinationFieldsData();
@@ -33,6 +36,7 @@ var ChooseFields = React.createClass({
                 });
             } catch (err) {}
         });
+        appActions.loadMappingFieldsData();
     },
     
     updateSourceFields: function(data) {
@@ -45,33 +49,83 @@ var ChooseFields = React.createClass({
             destinationFieldsData: data.destinationFieldsData
         });
     },   
+    updateFieldMappingStore: function(data) {
+        this.setState({
+            mappingFieldsData: data.mappingFieldsData
+        });
+    },   
+
     getOptions: function(sourceField){    
     var options = [];
     $.map(this.state.destinationFieldsData, function(item, i) {
-            if(sourceField.type == item.type){              
-               options.push({value:item.fieldName, label:item.displayName || item.fieldName})
+            if(item.mappable && sourceField.type == item.type){              
+               options.push({value:item.uniqueFieldName, label:item.displayName || item.uniqueFieldName})
             }              
          });
      return options
     }, 
+    checkAll: function() {
+
+    },
+    handleNext: function() {
+        this.props.eventHandlers.chooseFields(this.state.mappingFieldsData);
+    },
+    selectAll: function(event){
+        this.forceUpdate();
+    },
+    handleToggle: function(item, event) {
+        var mapping = [];
+        if(event.target.checked) {
+            var mappingObject = {
+                sourceField: item,
+                destinationField: null
+            }
+            mapping = this.state.mappingFieldsData.concat(mappingObject);
+        }
+        else
+        {
+            mapping =  _.reject(this.state.mappingFieldsData, function(v){ return v.sourceField.uniqueFieldName == item.uniqueFieldName;});
+        }
+
+        this.setState({
+                mappingFieldsData: mapping
+            });
+        this.forceUpdate();
+    },
+    handleSelectToggle: function(sourceField, destinationFieldName) {
+        var destinationField = _.find(this.state.destinationFieldsData, {uniqueFieldName: destinationFieldName});
+        var mapping = _.find(this.state.mappingFieldsData, function(v){ return sourceField.uniqueFieldName == v.sourceField.uniqueFieldName});
+        if(mapping) {
+            mapping.destinationField = destinationField;
+            this.forceUpdate();
+        }
+    },
     render: function() {
         var rows = [];
         if (this.state.destinationFieldsData && this.state.sourceFieldsData) {                    
            $.map(this.state.sourceFieldsData, function(item, i) {
-                var options = this.getOptions(item);                
-                rows.push(<tr key={item.fieldName}>
-                    <td>
-                        <input value={item.fieldName} aria-label="Field1" type="checkbox" onChange = {this.props.eventHandlers.selectFieldMapping}/>
-                    </td>
-                    <td>
-                        <div className="table_cell">
-                            {item.displayName}
-                        </div>
-                    </td>
-                    <td>                   
-                    <CustomSelect options={options} value="value" label="label" data={{sourceField:item.fieldName}} handleChange = {this.props.eventHandlers.updateFieldMappings}/>
-                    </td>
-                </tr>);
+                var options = this.getOptions(item);
+                if(item.mappable) {
+                    var selected = _.some(this.state.mappingFieldsData, function(v) { return item.uniqueFieldName == v.sourceField.uniqueFieldName});
+                    var mapping = _.find(this.state.mappingFieldsData, function(v) { return item.uniqueFieldName == v.sourceField.uniqueFieldName});
+                    var value = "";
+                    if(mapping && mapping.destinationField) {
+                        value = mapping.destinationField.uniqueFieldName;
+                    }
+                    rows.push(<tr key={item.uniqueFieldName}>
+                        <td>
+                            <input value={item.uniqueFieldName} aria-label="Field1" type="checkbox" checked={selected} onChange={this.handleToggle.bind(this, item)}/>
+                        </td>
+                        <td>
+                            <div className="table_cell">
+                                {item.displayName}
+                            </div>
+                        </td>
+                        <td>                   
+                        <CustomSelect initialOption={value} options={options} value="value" label="label" data={item} handleChange={this.handleSelectToggle}/>
+                        </td>
+                    </tr>);
+                }                
             }.bind(this));
         }
 
@@ -95,6 +149,7 @@ var ChooseFields = React.createClass({
                         <thead>
                             <tr>
                                 <th>
+                                    <input type="checkbox" checked={this.checkAll()} onChange={this.selectAll} />
                                      {this.props.i18nLib.t('wizard.map_fields.import_update')}
                                 </th>
                                 <th>
@@ -112,7 +167,7 @@ var ChooseFields = React.createClass({
                 </div>
                 <div className="buttons">
                     <button className="btn btn-warning navbar-btn btn-custom" type="button">{this.props.i18nLib.t('wizard.map_fields.save')}</button>&nbsp;
-                    <button className="btn btn-success navbar-btn btn-custom" type="button" onClick={this.props.eventHandlers.chooseFields}>{this.props.i18nLib.t('wizard.map_fields.next')}</button>
+                    <button className="btn btn-success navbar-btn btn-custom" type="button" onClick={this.handleNext}>{this.props.i18nLib.t('wizard.map_fields.next')}</button>
                 </div>
                 </div>
             ); } }); 
--- import-core/import-ui/src/main/webapp/client/scripts/components/wizard/custom-select.jsx
+++ import-core/import-ui/src/main/webapp/client/scripts/components/wizard/custom-select.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var CustomSelect = React.createClass({
      getInitialState: function() {
          return {
-             value: ''
+             value: this.props.initialOption
          }
      },
      change: function(event){
