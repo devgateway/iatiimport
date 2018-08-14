ba0e80448dd8340ed662f90a14b0975cb4cb93b4
Gerald Mutuhu <geraldmutuhu@gmail.com>
2018-08-14 13:54:49 +0300

    IATIIMPORT-65 DataSource Admin - FE changes
    IATIIMPORT-65 DataSource Admin - FE changes

---
+++ import-core/import-ui/src/main/webapp/client/scripts/stores/DataSourceStore.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var Reflux = require('reflux');
+var request = require('superagent');
+var appConfig = require('./../conf');
+var formActions = require('./../actions/form');
+
+
+var DataSourceStore = Reflux.createStore({
+
+  init: function() {
+    this.listenTo(formActions.loadDataSource, this.handleLoadDataSource);
+    this.listenTo(formActions.updateDataSource, this.handleUpdateDataSource);
+  },
+  handleUpdateDataSource: function(data) {
+    var self = this;
+    $.ajax({
+    	headers: { 
+    		'Accept': 'application/json',
+    		'Content-Type': 'application/json' 
+    	},
+    	url: '/importer/data-source',
+    	data: JSON.stringify(data),
+    	error: function() {    	    	  
+    		formActions.updateDataSource.failed(data);
+    	},
+    	dataType: 'json',
+    	success: function(data) {
+    		formActions.updateDataSource.completed(data);    	        
+    	},
+    	type: 'POST'
+    }); 
+  },
+  handleLoadDataSource: function() {
+    var self = this;    
+    $.ajax({
+        url: '/importer/data-source',        
+        error: function() {        	
+        	formActions.loadDataSource.failed();
+        },
+        dataType: 'json',
+        success: function(data) {        	
+        	formActions.loadDataSource.completed(data);
+        },
+        type: 'GET'
+     }); 
+  }
+});
+
+module.exports = DataSourceStore;
