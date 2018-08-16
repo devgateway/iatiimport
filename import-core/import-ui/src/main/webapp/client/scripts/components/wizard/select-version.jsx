edc3766ee46f4b14cd5a9ee9f34a59fcd4c652f5
Gerald Mutuhu <geraldmutuhu@gmail.com>
2018-08-16 15:47:05 +0300

    IATIIMPORT-69  select version component
    IATIIMPORT-69  select version component

---
+++ import-core/import-ui/src/main/webapp/client/scripts/components/wizard/select-version.jsx
@@ -0,0 +1,54 @@
+var React = require('react');
+var _ = require('lodash/dist/lodash.underscore');
+var fileStore = require('./../../stores/FileStore');
+var moment = require('moment');
+var reactAsync = require('react-async');
+var Reflux = require('reflux');
+var appActions = require('./../../actions');
+var appConfig = require('./../../conf');
+var DataSourceStore = require('./../../stores/DataSourceStore');
+var constants = require('./../../utils/constants');
+var formActions = require('./../../actions/form');
+var SelectVersion = React.createClass({
+    mixins: [Reflux.ListenerMixin
+    ],
+    getInitialState: function() {
+       return {};
+    },
+    componentDidMount: function() {  
+        this.props.eventHandlers.updateCurrentStep(constants.SELECT_VERSION);
+    }, 
+    handleNext: function () {
+        this.props.eventHandlers.initAutomaticImport();
+    },
+    render: function() {      
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading"><strong>{this.props.i18nLib.t('select_version.title')}</strong></div>
+                <div className="panel-body">
+                {this.props.versions &&
+                    <div>
+                     <label>{this.props.i18nLib.t('select_version.currently_importing')} {this.props.currentVersion}</label> <br/>  
+                      {this.props.versions.filter(v => v != this.props.currentVersion).length > 0 &&
+                          <div>
+                          <label>{this.props.i18nLib.t('select_version.other_versions_available')}</label> <br/>                    
+                           <ul className="list-unstyled workflow-selector">
+                               {this.props.versions.filter(v => v != this.props.currentVersion).map(function(version){
+                                  return (<li className="workflow-link">{version}</li>);  
+                               })}
+                           </ul>      
+                            <br/>
+                           {this.props.i18nLib.t('select_version.import_will_repeat')}                            
+                          </div>
+                      }                     
+                    </div>
+                }
+                </div>
+                <div className="buttons">
+                    <button className="btn btn-success navbar-btn btn-custom" type="button" onClick={this.handleNext}>{this.props.i18nLib.t('data_source.next')}</button>
+                </div>
+            </div>
+            );
+    } 
+}); 
+module.exports = SelectVersion;
