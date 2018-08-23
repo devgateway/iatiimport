482f39cda2274d3d5db0492ffa2310956a3f1641
Julian de Anquin <jdeanquin@developmentgateway.org>
2018-08-23 18:29:55 -0300

    IATIIMPORT-99 filter reporting orgs by name

--- import-core/import-rest/src/main/java/org/devgateway/importtool/rest/DataSourceController.java
+++ import-core/import-rest/src/main/java/org/devgateway/importtool/rest/DataSourceController.java
@@ -2,6 +2,7 @@ package org.devgateway.importtool.rest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,6 +13,7 @@ import org.devgateway.importtool.services.DataSourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,9 +37,16 @@ class DataSourceController {
 		return new ResponseEntity<>(dataSourceService.save(ds), HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value="reporting-orgs")
-	ResponseEntity<List<ReportingOrganization>> getOrganizations(HttpServletRequest request) {
-		return new ResponseEntity<>(reportingOrgRepository.findAll(), HttpStatus.OK);
+	@RequestMapping(method = RequestMethod.GET, value={"/reporting-orgs", "/reporting-orgs/{orgName}"})
+	ResponseEntity<List<ReportingOrganization>> getOrganizations(HttpServletRequest request,
+																 @PathVariable Optional<String> orgName) {
+		List<ReportingOrganization> reportingOrganizations;
+		if (orgName.isPresent()) {
+			reportingOrganizations = reportingOrgRepository.findByNameStartingWith(orgName.get());
+		} else {
+			reportingOrganizations = reportingOrgRepository.findAll();
+		}
+		return new ResponseEntity<>(reportingOrganizations, HttpStatus.OK);
 	}
 	@RequestMapping(method = RequestMethod.GET, value="reporting-orgs/with-updates")
 	ResponseEntity<List<ReportingOrganization>> getOrganizationsWithUpdates(HttpServletRequest request) {
