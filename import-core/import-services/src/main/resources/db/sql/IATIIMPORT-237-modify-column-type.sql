6ec8fedd724450706c7e25ad2b42348834a95679
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2019-02-27 11:42:26 -0300

    IATIATIIMPORT-237 add if exists to drop constraint

--- import-core/import-services/src/main/resources/db/sql/IATIIMPORT-237-modify-column-type.sql
+++ import-core/import-services/src/main/resources/db/sql/IATIIMPORT-237-modify-column-type.sql
@@ -1,7 +1,7 @@
 SET bytea_output = 'hex';
 create table uploaded_file2 as select id,author,created_date,file_data,file_name,CAST( substring(CAST ( session_id AS text) from 3) AS uuid) session_id,is_valid from uploaded_file;
-alter table project drop constraint  fk_l9ctv87fdco00dext3b3pypjf;
-alter table project drop constraint  fkabxrsnujmw12ikygyi0v0bsti;
+alter table project drop constraint if exists fk_l9ctv87fdco00dext3b3pypjf;
+alter table project drop constraint  if exists fkabxrsnujmw12ikygyi0v0bsti;
 drop table uploaded_file;
 create table uploaded_file as select * from uploaded_file2;
 alter table uploaded_file  add CONSTRAINT uploaded_file_pkey PRIMARY KEY (id);
