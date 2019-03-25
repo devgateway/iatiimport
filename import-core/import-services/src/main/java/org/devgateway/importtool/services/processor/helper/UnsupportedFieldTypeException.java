163fb36211448fafb490ddd9131b14b208a6a7b9
Gerald Mutuhu <geraldmutuhu@gmail.com>
2019-03-25 23:13:32 +0300

    IATIIMPORT-223 check if field type is supported before submitting to AMP
    IATIIMPORT-223 check if field type is supported before submitting to AMP

---
+++ import-core/import-services/src/main/java/org/devgateway/importtool/services/processor/helper/UnsupportedFieldTypeException.java
@@ -0,0 +1,15 @@
+package org.devgateway.importtool.services.processor.helper;
+
+public class UnsupportedFieldTypeException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4344327944896645291L;
+
+    public UnsupportedFieldTypeException(String message) {
+		super(message);
+	}
+	
+
+}
