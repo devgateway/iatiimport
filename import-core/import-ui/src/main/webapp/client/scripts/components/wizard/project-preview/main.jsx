ea7bb6cb4fbae8f4739653235ca45b450ba34837
Viorel Chihai <vchihai@developmentgateway.org>
2020-04-14 11:47:57 +0300

    IATIIMPORT-307: Fix tabIndex attribute and remove unused spaces

--- import-core/import-ui/src/main/webapp/client/scripts/components/wizard/project-preview/main.jsx
+++ import-core/import-ui/src/main/webapp/client/scripts/components/wizard/project-preview/main.jsx
@@ -14,13 +14,13 @@ var PolicyMarkers = require('./policy-markers');
 var ProjectPreview = React.createClass({
     getInitialState: function() {
        return {selectedProject: null};
-    }, 
+    },
     onClose: function() {
       this.props.closeProjectPreview();
     },
-    render: function () {    
+    render: function () {
         return (
-          <div className="modal fade in" id="projectPreview" tabindex="-1" role="dialog" aria-hidden="true" style={{display: 'block'}} >
+          <div className="modal fade in" id="projectPreview" tabIndex="-1" role="dialog" aria-hidden="true" style={{display: 'block'}} >
 			   <div ref="projectPreviewDialog" className="modal-dialog projectPreview">
 			    <div className="modal-content">
 			      <div className="modal-header">
@@ -28,12 +28,12 @@ var ProjectPreview = React.createClass({
 			        <h4 className="modal-title" >{this.props.i18nLib.t('project_preview.title')}</h4>
 			      </div>
 			      <div className="modal-body">
-			        <div className="preview_container"> 
-			        <Header {...this.props}/>				        
+			        <div className="preview_container">
+			        <Header {...this.props}/>
 			        <div className="preview_content">
 			        <div className="container-fluid">
-                     <div className="row">			        
-			           <div className="col-md-9 left-column">	
+                     <div className="row">
+			           <div className="col-md-9 left-column">
 			              <div className="main_group_container">
 			               <Identification {...this.props}/>
 			               <Planning {...this.props}/>
@@ -42,25 +42,25 @@ var ProjectPreview = React.createClass({
 			               <Sectors {...this.props}/>
 			               <PolicyMarkers {...this.props}/>
 			               <Locations {...this.props}/>
-			               <Transactions {...this.props}/>   
+			               <Transactions {...this.props}/>
 			              </div>
-			           </div>			        
-			            <div className="preview_summary col-md-offset-9"> 
+			           </div>
+			            <div className="preview_summary col-md-offset-9">
 			              <div className="summary_container">
 			                <FundingSummary {...this.props}/>
 			              </div>
                         </div>
-                    
+
                     </div>
 			        </div>
 			        </div>
-			        
-			        
-			        </div>				    
-			      
+
+
+			        </div>
+
 			    </div>
 			        <div className="modal-footer">
-                    <button type="button" className="btn btn-default btn-warning" data-dismiss="modal" onClick={this.onClose}>{this.props.i18nLib.t('project_preview.close')}</button>                  
+                    <button type="button" className="btn btn-default btn-warning" data-dismiss="modal" onClick={this.onClose}>{this.props.i18nLib.t('project_preview.close')}</button>
                   </div>
 			  </div>
 			</div>
