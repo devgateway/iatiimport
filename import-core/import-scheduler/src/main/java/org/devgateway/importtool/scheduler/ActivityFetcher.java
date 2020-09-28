bcb731058a2043e9034c36f42a5df1bc27e6f1e5
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2020-09-28 18:01:17 -0300

    IATIIMPORT-337 adjust new datastore

--- import-core/import-scheduler/src/main/java/org/devgateway/importtool/scheduler/ActivityFetcher.java
+++ import-core/import-scheduler/src/main/java/org/devgateway/importtool/scheduler/ActivityFetcher.java
@@ -2,87 +2,44 @@ package org.devgateway.importtool.scheduler;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.devgateway.importtool.dao.ProjectRepository;
 import org.devgateway.importtool.dao.ReportingOrgRepository;
-import org.devgateway.importtool.endpoint.Param;
-import org.devgateway.importtool.model.Project;
 import org.devgateway.importtool.services.ActivityFetchService;
-import org.devgateway.importtool.endpoint.DataFetchServiceConstants;
-import org.devgateway.importtool.services.processor.IATIProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.mapping;
-import static org.devgateway.importtool.services.processor.helper.IATIProcessorHelper.getStringFromElement;
 
 @Component
 public class ActivityFetcher {
 
     private static final Log log = LogFactory.getLog(ActivityFetcher.class);
-    //this is not ISO Format however iati site says it should be
-    private static final String ISO8601_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-    private static final SimpleDateFormat ISO8601_DATE_FORMAT_PARSER = new SimpleDateFormat(ISO8601_DATE_FORMAT);
 
     @Value("${IATIProcessor.default_country}")
     private String defaultCountry;
     @Autowired
     private ActivityFetchService activityFetchService;
-    @Autowired
-    private ProjectRepository projectRepository;
 
     @Autowired
     private ReportingOrgRepository reportingOrgRepository;
 
     @Scheduled(cron = "${ActivityFetcher.checkForUpdates.cron}")
     public void checkForUpdates() {
-        XPath xPath = XPathFactory.newInstance().newXPath();
-
-        StringBuilder query = new StringBuilder(IATIProcessor.DEFAULT_PATH_API);
-        getIatiIdentifiers().forEach((String reportingOrg, List<String> iatiIdentifiers) -> {
-            List<Param> params = DataFetchServiceConstants.getCommonParams(reportingOrg, defaultCountry);
-            params.add(new Param(DataFetchServiceConstants.IATI_IDENTIFIER_PARAMETER, iatiIdentifiers));
-            Document doc = activityFetchService.fetchFetchFromDataStore(reportingOrg, params);
-            NodeList activities;
-            try {
-                activities = (NodeList) xPath.compile(query.toString()).evaluate(doc, XPathConstants.NODESET);
-
-                for (int i = 0; i < activities.getLength(); i++) {
-                    Element element = (Element) activities.item(i);
-                    String lastUpdatedDateTime = element.getAttribute(IATIProcessor.LAST_UPDATED_DATE);
-                    projectRepository.updateLastUpdatedDateByProjectIdentifier(ISO8601_DATE_FORMAT_PARSER.parse(
-                            lastUpdatedDateTime),getStringFromElement(element, IATIProcessor.DEFAULT_ID_FIELD));
-                }
-            } catch (XPathExpressionException | ParseException e) {
-                log.error("Cannot fetch activities due to a malformed XPATH", e);
-            }
-        });
+        activityFetchService.checkForUpdates();
     }
 
-    private  Map<String, List<String>> getIatiIdentifiers() {
-        List<Project> p = projectRepository.findProjectLastSyncedDate();
-        return p.stream()
-                .collect(groupingBy(Project::getGroupingCriteria, mapping(Project::getProjectIdentifier,
-                        Collectors.toList())));
-    }
     @Scheduled(cron = "${ActivityFetcher.fetchActivitiesForSyncedReportingOrgs.cron}")
-    public void fetchActivitiesForSyncedReportingOrgs(){
+    public void fetchActivitiesForSyncedReportingOrgs() {
         List<String> gropingCriteriaList = reportingOrgRepository.getSyncedGroupingCriteria();
-        gropingCriteriaList.stream().forEach(reportingOrg ->activityFetchService.fetch(reportingOrg));
+        gropingCriteriaList.stream().forEach(reportingOrg -> activityFetchService.fetch(reportingOrg));
+    }
+
+    @EventListener
+    public void onApplicationEvent(ContextStartedEvent event) {
+        this.fetchActivitiesForSyncedReportingOrgs();
+        activityFetchService.checkForUpdates();
     }
 }
--- import-core/import-services/src/main/java/org/devgateway/importtool/endpoint/DataFetchServiceConstants.java
+++ import-core/import-services/src/main/java/org/devgateway/importtool/endpoint/DataFetchServiceConstants.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataFetchServiceConstants {
-    //this default url should come from the database
-    public static final String IATI_DATASTORE_DEFAULT_URL ="http://datastore.iatistandard.org/api/1/access/activity.xml";
+    public static final String IATI_DATASTORE_DEFAULT_URL = "https://iatidatastore.iatistandard.org/search/activity";
     public static final String IATI_ORGANIZATIONS_DEFAULT_URL = "https://iatiregistry.org/api/action/organization_list?" +
             "all_fields=true&include_extras=true";
-    public static final String RECIPIENT_COUNTRY_PARAMETER="recipient-country";
-    public static final String REPORTING_ORGANISATION_PARAMETER="reporting-org";
-    public static final String IATI_IDENTIFIER_PARAMETER="iati-identifier";
-    public static final String IATI_EXTRA_VERSION ="iati-extra:version";
+    public static final String PARAMETER_REPLACE_VALUE = "$";
+    public static final String REPORTING_ORGANISATION_PARAMETER = "reporting_org_ref:(" + PARAMETER_REPLACE_VALUE + ")";
+    public static final String RECIPIENT_COUNTRY_PARAMETER = "recipient_country_code:(" + PARAMETER_REPLACE_VALUE + ")";
+    public static final String IATI_IDENTIFIER_PARAMETER = "iati_identifier:(" + PARAMETER_REPLACE_VALUE + ")";
+    public static final String PARAM_AND_VALUE = " AND ";
+    public static final String IATI_EXTRA_VERSION = "iati-extra:version";
     public static final String ACTIVITIES_FILES_STORE = "ActivityFetcher.fileStore";
 
-    public static List<Param> getCommonParams(String reportingOrg,String defaultCountry) {
+    public static List<Param> getCommonParams(String reportingOrg, String defaultCountry) {
         List<Param> params = new ArrayList<>();
-        //SN should be taken from AMP
         params.add(new Param(DataFetchServiceConstants.RECIPIENT_COUNTRY_PARAMETER, defaultCountry));
         params.add(new Param(DataFetchServiceConstants.REPORTING_ORGANISATION_PARAMETER, reportingOrg));
         return params;
