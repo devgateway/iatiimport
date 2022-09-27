5bf64fd79870951e31ebedc9dd76d5d3f77c2e5e
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2022-09-27 13:50:50 +0300

    AMP-30350: Removed data source manager

--- import-core/import-services/src/main/java/org/devgateway/importtool/dao/DataSourceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.devgateway.importtool.dao;
-
-import org.devgateway.importtool.model.DataSource;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DataSourceRepository extends PagingAndSortingRepository<DataSource, Long> {
-
-
-}
